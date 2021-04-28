62de7829624bce2cea0b825512ca36ea254e1b47
jtambo99 <jtambo@live.com>
2021-04-28 16:49:03 +0200

    Integration front-back on createPetitions

--- frontend/src/petitions/CreatePetition.tsx
+++ frontend/src/petitions/CreatePetition.tsx
@@ -1,56 +1,110 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../Navbar.css'
 
 export interface createPetitionProps {
 
 }
+const createPetition = () => {
+
+}
 
 const CreatePetition: React.JSXElementConstructor<createPetitionProps> = () => {
 
-    const handleSubmit = (e: any) => {
+    // Form variables
+    const [title, setTitle] = useState("");
+    const [date, setDate] = useState("");
+    const [place, setPlace] = useState("");
+    const [hour, setHour] = useState("");
+    const [mail, setMail] = useState("");
+    const [isUrgent, setIsUrgent] = useState(false);
+    const [description, setDescription] = useState("");
+    const [isPending, setIsPending] = useState(false);
+    // TODO: Esta variable debe obtener el id del usuario logeado
+    //const [userId, setUserId] = useState("");
+    const userId = "608973ba40f1db3b48fc1044";
+
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
-        console.log("Submit");
+
+        setIsPending(true);
+
+        const result = await fetch(`${process.env.REACT_APP_BASEURL}/petitions`,
+            {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: title,
+                    userId: userId,
+                    body: description,
+                    place: place,
+                    targetDate: new Date(date),
+                    isUrgent: isUrgent
+                })
+            });
+
+        if (result) {
+            setIsPending(false);
+        }
+        console.log("Result: " + await result.text());
+
     }
 
     return (
         <div className="container-fluid">
             <form onSubmit={handleSubmit}>
                 <div className="mt-4">
-                    <input type="text" id="disabledTextInput" className="form-control" placeholder="Titulo..." required></input>
+                    <input type="text" id="disabledTextInput" className="form-control" placeholder="Titulo..."
+                        onChange={e => setTitle(e.target.value)} required></input>
                 </div>
                 <div className=" row mt-4">
                     <div className="col-6">
-                        <input type="date" id="disabledTextInput" className="form-control" placeholder="Fecha" required></input>
+                        <input type="date" id="disabledTextInput" className="form-control" placeholder="Fecha"
+                            onChange={e => setDate(e.target.value)} required></input>
                     </div>
                     <div className="col-6">
-                        <input type="text" id="disabledTextInput" className="form-control" placeholder="Lugar" required></input>
+                        <input type="text" id="disabledTextInput" className="form-control" placeholder="Lugar"
+                        onChange={e => setPlace(e.target.value)} required></input>
                     </div>
                 </div>
                 <div className=" row mt-4">
                     <div className="col-6">
-                        <input type="time" id="disabledTextInput" className="form-control" placeholder="Hora" required></input>
+                        <input type="time" id="disabledTextInput" className="form-control" placeholder="Hora" 
+                        onChange={e => setHour(e.target.value)} required></input>
                     </div>
                     <div className="col-6">
-                        <input type="email" id="disabledTextInput" className="form-control" placeholder="Correo electrónico" required></input>
+                        <input type="email" id="disabledTextInput" className="form-control" placeholder="Correo electrónico" 
+                        onChange={e => setMail(e.target.value)} required></input>
                     </div>
                 </div>
                 <div className=" row mt-4">
                     <div className="col">
-                        <input type="checkbox" id="disabledTextInput" className="form-check-input" placeholder="Urgente" required></input>
+                        <input type="checkbox" id="disabledTextInput" className="form-check-input" placeholder="Urgente" 
+                        onChange={e => setIsUrgent(e.target.checked)} required></input>
                         <label className="form-check-label ms-3">
                             Urgente
                         </label>
                     </div>
                 </div>
                 <div className="mt-4">
-                    <textarea id="TextArea" className="form-control" placeholder="Añadir descripción" required></textarea>
+                    <textarea id="TextArea" className="form-control" placeholder="Añadir descripción" 
+                    onChange={e => setDescription(e.target.value)} required></textarea>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col d-grid gap-2 d-md-flex justify-content-md-center">
-                        <button type="submit" className="btn navbar-azul text-light text-decoration-none mt-4">
-                            Añadir petición<i className="fas fa-plus ms-2"></i>
-                        </button>
+                        {!isPending &&
+                            <button type="submit" className="btn navbar-azul text-light text-decoration-none mt-4">
+                                Añadir petición<i className="fas fa-plus ms-2"></i>
+                            </button>
+                        }
+                        {isPending &&
+                            <button type="submit" className="btn navbar-azul text-light text-decoration-none mt-4">
+                                Añadiendo petición ...
+                            </button>
+                        }
                     </div>
                 </div>
             </form>
