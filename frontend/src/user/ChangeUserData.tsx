c34e575805ebc5246c9c7c2e6dcd8cf2ab43971e
JoseYag <josefelix.yag@gmail.com>
2021-05-03 15:56:05 +0200

    Integración user con imagenes y modificaciones api

--- frontend/src/user/ChangeUserData.tsx
+++ frontend/src/user/ChangeUserData.tsx
@@ -1,10 +1,67 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import base64 from 'react-native-base64';
+import useGetFetch from '../useGetFetch';
 
 const ChangeUserData = () => {
 
+    const [passwordAct, setPasswordAct] = useState("");
+    const [passwordNue, setPasswordNue] = useState("");
+    const [zone, setZone] = useState("");
+    //Cambiar por id de usuario
+    const id = "608ee32beb584f654c7dea6d";
+    const { data: user, error, isPending } = useGetFetch(`${process.env.REACT_APP_BASEURL}/user/` + id);
+    const passwordActual = (user as any).password;
+    let picData:any;
+
+    var { data: dataZones, error:error2, isPending:isPending2 } = useGetFetch(`${process.env.REACT_APP_BASEURL}/zone`);
+    
+    function getBase64(file: any) {
+        //console.log("pic "+file);
+        var reader = new FileReader();
+        reader.readAsDataURL(file[0]);
+        reader.onload = function () {
+          console.log("reader"+reader.result);
+          picData=reader.result;
+        };
+        reader.onerror = function (error) {
+          console.log('Error: ', error);
+        };
+     }
+
+
     // Accion de cambiar la contraseña
-    const changeDataButtonClicked = () => {
-        console.log("Cambiar la contraseña");
+    const changeDataButtonClicked = async () => { 
+        console.log("Cambiar datos");
+        /*console.log("contraseña actual "+passwordActual);
+        console.log("contraseña por user "+passwordAct);
+        console.log("contraseña nueva "+passwordNue);
+        console.log("pic "+picture);*/
+        //var file = new File([picture], picture, {type: 'image/jpeg'});
+        //const { data: picData, error, isPending } = await getBase64(file);
+        console.log("zona "+zone);
+        let data = picData && picData || "";
+        data = data.split("data:image/jpeg;base64,").pop();
+        console.log("picData "+picData);
+        console.log("data "+data);
+        if (passwordActual == passwordAct){
+            console.log("Las contraseñas coinciden");
+            const result = await fetch(`${process.env.REACT_APP_BASEURL}/user/` + id,
+            {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    password: passwordNue,
+                    picture: data,
+                    sanitaryZone: zone
+                })
+            })
+        } else{
+            console.log("Las contraseñas no coinciden");
+        }
+        
     }
 
     return (
@@ -27,14 +84,18 @@ const ChangeUserData = () => {
                                     <label htmlFor="oldPass" className="input-group-text">
                                         Contraseña actual
                                                 </label>
-                                    <input type="password" id="oldPass" className="form-control" />
+                                    <input type="password" id="oldPass" className="form-control" onChange={e => setPasswordAct(e.target.value)} required/>
+                                    <div className="invalid-feedback">
+                                        Looks good!
+                                    </div>
+                                    
                                 </div>
                                 {/* Contraseña nueva */}
                                 <div className="input-group mt-3">
                                     <label htmlFor="newPass" className="input-group-text">
                                         Contraseña nueva
                                                 </label>
-                                    <input type="password" id="newPass" className="form-control" />
+                                    <input type="password" id="newPass" className="form-control"  onChange={e => setPasswordNue(e.target.value)}/>
                                 </div>
                             </div>
                         </div>
@@ -45,23 +106,51 @@ const ChangeUserData = () => {
                                     <label htmlFor="zonasanitariaselect" className="input-group-text">
                                         Zona sanitaria
                                                 </label>
-                                    <select id="zonasanitariaselect" className="form-control" value="id1">
-                                        <option value="id1">El Arrabal</option>
-                                        <option value="id2">Huesca</option>
-                                        <option value="id3">Ejea</option>
-                                        <option value="id4">Huesca Rural</option>
-                                    </select>
+                                                {/*<select id="zonasanitariaselect" className="form-control" value="id1">
+                                                    <option value="id1">El Arrabal</option>
+                                                    <option value="id2">Huesca</option>
+                                                    <option value="id3">Ejea</option>
+                                                    <option value="id4">Huesca Rural</option>
+                                                </select>*/}
+                                                <select id="zonasanitariaselect" className="form-control" onChange={e => setZone(e.target.value)}>
+                                                {dataZones.map((zone: any) => (
+                                                        
+                                                    
+                                                        <option key={zone._id} value={zone._id}>{zone.name}</option>
+                                                    
+                                                        
+                                                ))}
+                                                </select>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        {/* Cambio de zona imagen */}
+                        <div className="card mt-3">
+                            <div className="card-header">
+                                Cambio de imagen
+                            </div>
+                            <div className="card-body">
+                                <div className="input-group">
+                                    <input type="file" name="image" id="formFile" className="form-control-file"  accept=".jpg, .jpeg, .png" onChange={e => getBase64(e.target.files)}/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                        <button type="button" className="btn btn-success" onClick={changeDataButtonClicked} data-bs-dismiss="modal">Guardar</button>
+                        <button type="button" className="btn btn-success" onClick={changeDataButtonClicked} data-bs-dismiss="modal" >Guardar</button>
                     </div>
                 </div>
             </div>
+                
+            
+
+
         </div>
+
+        
+        
     )
 }
 
--- frontend/src/user/UserProfile.tsx
+++ frontend/src/user/UserProfile.tsx
@@ -1,30 +1,45 @@
-import React, { JSXElementConstructor } from 'react'
+import React, { JSXElementConstructor , useEffect, useState} from 'react'
+import { useParams } from "react-router";
 import PetitionListComponent from '../petitions/PetitionListComponent';
 import PostListComponent from '../posts/PostListComponent';
 import ChangeUserData from './ChangeUserData';
+import useGetFetch from '../useGetFetch';
+import base64 from 'react-native-base64'
+
+
 
 interface UserProfileProps {
-
+    
 }
 
 const UserProfile: JSXElementConstructor<UserProfileProps> = () => {
 
     // Variables del usuario
-    const username = "elPepe";
-    const email = "elpepe@unizar.es";
-    const zonaSanitaria = "El Arrabal";
+    
+    //Cambiar por id de usuario
+    const id = "608ee32beb584f654c7dea6d";
+    const { data: user, error, isPending } = useGetFetch(`${process.env.REACT_APP_BASEURL}/user/` + id);
 
-    const petitions = [
-        { title: 'Titulo de peticion', task: 'Comprar cerveza', address: 'Calle Alfonso Artero', author: 'ElJosé', id: 1, done: false },
-        { title: 'Titulo de peticion', task: 'Comprar cerveza', address: 'Calle Alfonso Artero', author: 'ElJosé', id: 2, done: true },
-        { title: 'Titulo de peticion', task: 'Comprar cerveza', address: 'Calle Alfonso Artero', author: 'ElJosé', id: 3, done: false }
-    ];
+    const username = (user as any).name;
+    console.log('username'+username);
+    const email = (user as any).email;
+    const zonaSanitariaID = (user as any).zonaSanitaria;
+    const picture =(user as any).picture;
 
-    const posts = [
-        { title: 'Titulo del post', body: 'lorem ipsum...', author: 'ElJosé', id: 1, likes: 17 },
-        { title: 'Titulo del post', body: 'lorem ipsum...', author: 'ElJosé', id: 2, likes: 17 },
-        { title: 'Titulo del post', body: 'lorem ipsum...', author: 'ElJosé', id: 3, likes: 17 }
-    ];
+    
+    const picture2 = "data:image/jpeg;base64,"+picture;
+    
+
+    //const { data: zona, error:error2, isPending:isPending2 } = useGetFetch(`${process.env.REACT_APP_BASEURL}/zone/` + zonaSanitariaID);
+    var { data: dataPosts, error:error2, isPending:isPending2 } = useGetFetch(`${process.env.REACT_APP_BASEURL}/user/` + id + `/posts`);
+
+    console.log(dataPosts);
+    
+
+    const { data: dataPetitions, error:error3, isPending:isPending3 } = useGetFetch(`${process.env.REACT_APP_BASEURL}/user/` + id + `/petitions`);
+
+    //const zonaSanitaria = (zona as any).name;
+    
 
     return (
         <div className="container-fluid d-flex justify-content-center card">
@@ -49,27 +64,34 @@ const UserProfile: JSXElementConstructor<UserProfileProps> = () => {
                     </div>
                     {/* Columna de la imagen del usuario */}
                     <div className="col-3 d-flex justify-content-end">
-                        <img src="http://www.battelldentureclinic.com/app/webroot/images/userImages/8.jpg"
-                            alt="user" className="rounded-circle w-50 border border-secondary border-2" />
+                        
+                        {/*<img src="http://www.battelldentureclinic.com/app/webroot/images/userImages/8.jpg"
+                            alt="user" className="rounded-circle w-50 border border-secondary border-2" />*/}
+                            <img src={picture2} alt="user" className="rounded-circle w-50 border border-secondary border-2" />
+                            {/*<img src={data}
+                            alt="user" className="rounded-circle w-50 border border-secondary border-2" />*/}
+                        
                     </div>
-                </div>
+                </div> 
+                
                 <div className="container-fluid px-md-5 px-3 mt-md-4 mt-3 d-flex justify-content-center">
                     {/* Boton de cambio de contraseña */}
                     <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                         Cambiar datos
                     </button>
+                   
                     {/* Modal de cambio */}
                     <ChangeUserData />
                 </div>
                 {/* Lista de Posts */}
                 <div className="container-fluid mt-md-4 mt-3">
                     <p className="display-6">Posts:</p>
-                    <PostListComponent posts={posts} />
+                    <PostListComponent posts={dataPosts} />
                 </div>
                 {/* Lista de Peticiones de ayuda */}
                 <div className="container-fluid mt-md-4 mt-3">
                     <p className="display-6">Peticiones de ayuda:</p>
-                    <PetitionListComponent petitions={petitions} />
+                    <PetitionListComponent petitions={dataPetitions} />
                 </div>
             </div>
         </div>
