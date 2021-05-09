3a674d611ce09bee41d720ed5783117489eb36c5
jtambo99 <jtambo@live.com>
2021-05-09 11:57:11 +0200

    Protected Petitions and Posts modifications routes (POST, PUT, DELETE)

--- backend/src/route/petitionsRoute.ts
+++ backend/src/route/petitionsRoute.ts
@@ -8,12 +8,12 @@ const petitionsRoute = Express.Router();
 // All petitions
 petitionsRoute.route("/")
     .get(petitionsController.getPetitions)
-    .post(passport.authenticate('jwt', {session: false}), petitionsController.petitionsCreate);
+    .post(passport.authenticate('jwt', { session: false }), petitionsController.petitionsCreate);
 
 // Especific petitionx
 petitionsRoute.route("/:petitionId")
     .get(petitionsController.readOnePetition)
-    .put(petitionsController.updateOnePetition)
-    .delete(petitionsController.deleteOnePetition);
+    .put(passport.authenticate('jwt', { session: false }), petitionsController.updateOnePetition)
+    .delete(passport.authenticate('jwt', { session: false }), petitionsController.deleteOnePetition);
 
 export default petitionsRoute;
