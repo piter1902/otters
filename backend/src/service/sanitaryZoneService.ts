8ebad834b39cd87c8d02958c7195c88101957cf6
Pedro Allué <pedro.allue.tamargo@gmail.com>
2021-05-12 08:42:01 +0200

    Trying to use another XLSX package

--- backend/src/service/sanitaryZoneService.ts
+++ backend/src/service/sanitaryZoneService.ts
@@ -3,7 +3,7 @@ import exceljs from 'exceljs';
 import fs from 'fs';
 import SanitaryZone from '../models/SanitaryZone';
 import logger from '@poppinss/fancy-logs';
-import { Mongoose } from 'mongoose';
+import XLSX from 'xlsx';
 import Utils from '../Utils';
 
 const URI = "https://transparencia.aragon.es/sites/default/files/documents/"
@@ -89,6 +89,71 @@ const parseXLSX = async (filename: string, zones: ZoneData[]) => {
     }
 }
 
+// Read file with another package
+const parseXLSX2 = async (filename: string, zones: ZoneData[]) => {
+    // const xlsxFile = await xlsx.readFile(file);
+    try {
+        const workbook = XLSX.readFile(filename);
+        const workSheet = workbook.Sheets[workbook.SheetNames[0]];
+        logger.info("Procesando File " + filename);
+        for (let i = 28; ; i++) {
+            // const data = workSheet.getRow(i);
+            const name = workSheet['B' + i] ? workSheet['B' + i].t.v : undefined;
+            if (filename == "20210418_casos_confirmados_zbs.xlsx") {
+                // Es uno de los días que no procesa bien
+                logger.warn("B" + i + "  " + name || "nombre??");
+            }
+            if (name != undefined && name && name.trim() != ""
+                && !name.trim().toUpperCase().match("ZBS")   // No es una fila de ZBS sin identificar...
+                && !name.trim().toUpperCase().match("Zona")  // No es una fila de ZBS sin identificar...
+                && !name.trim().toUpperCase().match("TOTAL") // No sea fila de total
+                && name.match("^[a-zA-Z].*$")) {             // Para evitar valores numéricos extraños se pide que el nombre empiece por una letra            
+                // Parsear cuando se sepa que tiene que tener un valor
+                const possitives = parseInt(workSheet['C' + i] ? workSheet['C' + i].v : "0");
+                if (filename == "20210418_casos_confirmados_zbs.xlsx") {
+                    // Es uno de los días que no procesa bien
+                    logger.warn("C" + i + "  " + possitives?.toString() || "positivos??");
+                }
+                if (possitives != undefined && !isNaN(possitives)) {
+                    // Es una zona de salud válida (número de positivos es válido)
+                    zones.push({
+                        name,
+                        possitives
+                    });
+                }
+            } else if (name != undefined && name.trim().toUpperCase().match("TOTAL")) {
+                // Fila final (total)
+                // Guardar los datos como si fuera aragon
+                const possitives = parseInt(workSheet['C' + i] ? workSheet['C' + i].v : "0");
+                if (possitives != undefined && !isNaN(possitives)) {
+                    logger.watch("File: " + filename + " Zona sanitaria aragon encontrada. NPos = " + possitives);
+                    zones.push({
+                        name: "Aragon",
+                        possitives
+                    });
+                }
+                break;
+            } else if (name == undefined) {
+                // Casilla vacía -> fin del bucle
+                break;
+            }
+        }
+        // Comprobación de si existe aragon
+        if (zones.findIndex(z => z.name === "Aragon") == -1) {
+            // No existe -> se crea
+            let pos = 0;
+            zones.forEach(z => pos += z.possitives);
+            logger.watch("File: " + filename + " Zona sanitaria aragon no encontrada. Calculando NPos = " + pos);
+            zones.push({
+                name: "Aragon",
+                possitives: pos
+            });
+        }
+    } catch (error) {
+        logger.error(`File: ${filename} -- Error: ${error}`);
+    }
+}
+
 // Parse file
 // Datos desde la línea 27 a la 85 (no incluidas)
 const readAndParseData = async (fileName: string, date: Date) => {
@@ -97,7 +162,7 @@ const readAndParseData = async (fileName: string, date: Date) => {
     const zones: ZoneData[] = [];
     // parseCSV(file, zones);
     // Usar XLSX porque con el CSV hay condicion de carrera y puede que no se escriba antes de leerse
-    await parseXLSX(fileName, zones);
+    await parseXLSX2(fileName, zones);
     logger.info("Longitud de zones en fecha " + date.toISOString() + ": " + zones.length);
     // Update mongo model
     for (let z of zones) {
@@ -128,7 +193,7 @@ const readAndParseData = async (fileName: string, date: Date) => {
                 const zoneToUpdate = Object.assign({}, zone);
                 delete zoneToUpdate._id;
 
-                await SanitaryZone.findOneAndUpdate({_id: zone._id}, zoneToUpdate).exec();
+                await SanitaryZone.findOneAndUpdate({ _id: zone._id }, zoneToUpdate).exec();
             }
         } else {
             // Doesn't exists -> create new zone
