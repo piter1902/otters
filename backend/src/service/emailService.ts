6ce874932ac8de93d275c5e6a68e24b5d09e7c84
jesusvs7 <jesusvillacampasagaste@gmail.com>
2021-05-17 19:57:20 +0200

    creation of notifications via email

--- backend/src/service/emailService.ts
+++ backend/src/service/emailService.ts
@@ -1,5 +1,6 @@
 import logger from '@poppinss/fancy-logs';
 import Nodemailer from 'nodemailer';
+import userController from '../controller/userController';
 
 
 const sendVerificationEmail = async (user: any) => {
@@ -30,6 +31,87 @@ const sendVerificationEmail = async (user: any) => {
     });
 }
 
+const sendSomeoneAssignedPetition = async (user:any, petition: any) => {
+    const transporter = Nodemailer.createTransport({
+        //pool: true,
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER!,
+            pass: process.env.EMAIL_PASS!
+        }
+    })
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER!,
+        to: user.email,
+        subject: "Someone has been assigned to your petition",
+        html: `
+            <div>
+                <p>
+                    Hello! <br>
+                    Someone has been assigned to your petition <a href="${process.env.BACK_URI ?? "http://localhost:3000"}/peticionDetalle/${petition._id}">${petition.title}</a> <br>
+                </p>
+            </div>
+        ` //TODO: Hacerlo con la variable de frontend de heroku
+    });
+}
+
+const sendSomeoneCommentedPost = async (user:any, post: any) => {
+    const transporter = Nodemailer.createTransport({
+        //pool: true,
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER!,
+            pass: process.env.EMAIL_PASS!
+        }
+    })
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER!,
+        to: user.email,
+        subject: "Someone has commented your post",
+        html: `
+            <div>
+                <p>
+                    Hello! <br>
+                    Someone has commented your post <a href="${process.env.BACK_URI ?? "http://localhost:3000"}/postDetalle/${post._id}">${post.title}</a> <br>
+                </p>
+            </div>
+        ` //TODO: Hacerlo con la variable de frontend de heroku
+    });
+}
+
+const fromQueueToAssigned = async (user:any, petition: any) => {
+    const transporter = Nodemailer.createTransport({
+        //pool: true,
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER!,
+            pass: process.env.EMAIL_PASS!
+        }
+    })
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER!,
+        to: user.email,
+        subject: "Someone has commented your post",
+        html: `
+            <div>
+                <p>
+                    Hello! <br>
+                    You've been promoted and now you are assigned to this petition <a href="${process.env.BACK_URI ?? "http://localhost:3000"}/peticionDetalle/${petition._id}">${petition.title}</a> <br>
+                </p>
+            </div>
+        ` //TODO: Hacerlo con la variable de frontend de heroku
+    });
+}
+
 export default {
-    sendVerificationEmail
+    sendVerificationEmail,
+    sendSomeoneAssignedPetition,
+    sendSomeoneCommentedPost,
+    fromQueueToAssigned
 }
